208b6f6b7386d0de16cd5fba64bc98af64fcefd8
ymanseur <ymanseur94@gmail.com>
2015-03-27 13:55:45 -0400

    Update Card.java
    Added general methods for what to expect from the card class

--- src/Backend/Card.java
+++ src/Backend/Card.java
@@ -2,12 +2,30 @@ package Backend;
 
 /**
  * @author MSmarsch
+ * @author Yacine
  */
 public class Card {
-    public int color;
-    public int pattern;
-    public int shape;
-    public int count;
+    //Possible colors
+    public final static int RED = 0;
+	public final static int GREEN = 1;
+	public final static int PURPLE = 2;
+	//Possible patterns
+	public final static int SOLID = 0;
+	public final static int STRIPED = 1;
+	public final static int OUTLINED = 2;
+	//Possible shapes
+	public final static int DIAMOND = 0;
+	public final static int SQUIGGLE = 1;
+	public final static int OVAL = 2;
+	//Possible counts
+	public final static int ONE = 0;
+	public final static int TWO = 1;
+	public final static int THREE = 2;
+    
+    public final int color;
+    public final int pattern;
+    public final int shape;
+    public final int count;
 
     public Card(int color, int pattern, int shape, int count)
     {
@@ -17,8 +35,62 @@ public class Card {
         this.count = count;
     }
 
+    /** need to ask matt about what he was trying to do with this constructor
     public Card(int i)
     {
         this.color =
+    }
+    */
+    
+    public int getColor() {
+    	return this.color;
+    }
+    
+    public String getColorAsString() {
+    	switch (color) {
+	    	case RED:		return "Red";
+	    	case GREEN:		return "Green";
+	    	case PURPLE:	return "Purple";
+	    	default:		return null;
+    	}
+    }
+    
+    public int getPattern() {
+    	return this.pattern;
+    }
+    
+    public String getPatternAsString() {
+    	switch (pattern) {
+	    	case SOLID:		return "Solid";
+	    	case STRIPED:	return "Striped";
+	    	case OUTLINED:	return "Outlined";
+	    	default:		return null;
+    	}
+    }
+    
+    public int getShape() {
+    	return this.shape;
+    }
+    
+    public String getShapeAsString() {
+    	switch (shape) {
+	    	case DIAMOND:	return "Diamond";
+	    	case SQUIGGLE:	return "Squiggle";
+	    	case OVAL:		return "Oval";
+	    	default:		return null;
+    	}
+    }
+    
+    public int getCount() {
+    	return this.count;
+    }
+    
+    public String getCountAsString() {
+    	switch (count) {
+	    	case ONE:	return "One";
+	    	case TWO:	return "Two";
+	    	case THREE:	return "Three";
+	    	default:	return null;
+    	}
     }
 }
