c5482fefecaa9275890c6189c25fc208c40bafa9
ymanseur <ymanseur94@gmail.com>
2015-05-01 07:03:17 -0400

    Create Scoring.java

---
+++ src/Backend/Scoring.java
@@ -0,0 +1,42 @@
+package backend;
+import java.util.*;
+
+public class Scoring {
+	public List<Player> players;
+	
+	public Scoring(List<Player> players){
+		this.players = players;
+		
+		// initialize scores of all players to zero
+		resetScores();
+	}
+	
+	public void correctSet(int id){
+		for(int i=0; i<players.size(); i++){
+			Player p = players.get(i);
+			if(p.getId() == id)
+				p.addScore();
+		}
+	}
+	
+	public void incorrectSet(int id){
+		for(int i=0; i<players.size(); i++){
+			Player p = players.get(i);
+			if(p.getId() == id)
+				p.subtractScore();
+		}
+	}
+	
+	
+	public void resetScores(){
+		for (int i = 0; i < this.players.size(); i++)
+			this.players.get(i).resetScore();
+	}
+
+	public String toString(){
+		String text = "";
+		for(int i=0; i<players.size(); i++)
+			text += players.get(i).toString();
+		return text;
+	}
+}
