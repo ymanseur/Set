5dd7480ca27b1b80e131c1b10c8550dce8f8eb2f
ymanseur <ymanseur94@gmail.com>
2015-05-01 07:02:12 -0400

    Update Deck.java

--- src/Backend/Deck.java
+++ src/Backend/Deck.java
@@ -1,4 +1,4 @@
-package Backend;
+package backend;
 
 /**
  * 
@@ -9,22 +9,25 @@ public class Deck {
 	public int cardsRemaining;
 	public Card[] deck;
 	
-	public Deck() {
+	public Deck(){
 		this.cardsRemaining = 81;
 		deck = new Card[this.cardsRemaining];
 		int cardCount = 0; // Number of cards created
 		
-		for(int color = 0; color <= 2; color++)
-			for(int pattern = 0; pattern <= 2; pattern++)
-				for(int shape = 0; shape <= 2; shape++)
-					for(int count = 0; count <= 2; count++) {
+		for(int color = 0; color <= 2; color++){
+			for(int pattern = 0; pattern <= 2; pattern++){
+				for(int shape = 0; shape <= 2; shape++){
+					for(int count = 0; count <= 2; count++){
 						deck[cardCount] = new Card(color,pattern,shape,count);
 						cardCount++;
 					}
+				}
+			}
+		}
 	}
-	
-	public void shuffle() {
-		for(int i = deck.length-1; i > 0; i--) {
+
+	public void shuffle(){
+		for(int i = deck.length-1; i > 0; i--){
 			int rand = (int)(Math.random()*(i+1));
 			Card temp = deck[i];
 			deck[i] = deck[rand];
@@ -32,16 +35,16 @@ public class Deck {
 		}
 	}
 	
-	public int cardsLeft() {
+	public int cardsLeft(){
 		return this.cardsRemaining;
 	}
 	
-	public Card dealCard() {
+	public Card dealCard(){
 		this.cardsRemaining--;
 		return deck[this.cardsRemaining];
 	}
 	
-	public boolean isEmpty() {
+	public boolean isEmpty(){
 		if(cardsRemaining == 0)
 			return true;
 		else
