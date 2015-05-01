30522bb1eb8fba0b4d5f1926850097a86d87e56a
ymanseur <ymanseur94@gmail.com>
2015-05-01 07:03:40 -0400

    unfinished

---
+++ src/Backend/Game.java
@@ -0,0 +1,35 @@
+package backend;
+
+import java.util.*;
+
+/**
+ * 
+ * @author Yacine
+ *
+ */
+
+
+public class Game {
+	final List<Player> players;
+	String name;
+	final int numPlayers;
+	Board board;
+	Scoring scoring;
+	
+	public Game(String name, int numPlayers){
+		players = new ArrayList<>();
+		this.name = name;
+		this.numPlayers = numPlayers;
+	}
+	
+	public String initializeGame(){
+		board = new Board();
+		board.refillBoard();
+		scoring = new Scoring(players);
+		// decide on way to simply send the board between client/server
+		// probably as a string
+		return "";
+	}
+	
+
+}
