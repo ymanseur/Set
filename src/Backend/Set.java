b24ddcab91bf7e0fa4d13a2e121c00869c4879c8
ymanseur <ymanseur94@gmail.com>
2015-05-01 07:02:53 -0400

    Create Set.java

---
+++ src/Backend/Set.java
@@ -0,0 +1,71 @@
+package backend;
+/**
+ * 
+ * @author Yacine
+ *
+ */
+
+public class Set {
+	Card[] cards;
+	
+	Set(Card c1, Card c2, Card c3){
+		cards[0] = c1;
+		cards[1] = c2;
+		cards[2] = c3;
+	}
+	
+	public Card getCard(int index){
+		return cards[index];
+	}
+	
+	public boolean checkValidSet(){
+		if(colorsEqualOrOpposite()
+				&& patternsEqualOrOpposite()
+				&& shapesEqualOrOpposite()
+				&& countsEqualOrOpposite())
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean colorsEqualOrOpposite(){
+		boolean b = (cards[0].getColor() == cards[1].getColor()
+				&& cards[1].getColor() == cards[2].getColor()
+				&& cards[0].getColor() == cards[2].getColor())
+				||(cards[0].getColor() != cards[1].getColor()
+				&& cards[1].getColor() != cards[2].getColor()
+				&& cards[0].getColor() != cards[2].getColor());
+		return b;
+	}
+	
+	public boolean patternsEqualOrOpposite(){
+		boolean b = (cards[0].getPattern() == cards[1].getPattern()
+				&& cards[1].getPattern() == cards[2].getPattern()
+				&& cards[0].getPattern() == cards[2].getPattern())
+				||(cards[0].getPattern() != cards[1].getPattern()
+				&& cards[1].getPattern() != cards[2].getPattern()
+				&& cards[0].getPattern() != cards[2].getPattern());
+		return b;
+	}
+	
+	public boolean shapesEqualOrOpposite(){
+		boolean b = (cards[0].getShape() == cards[1].getShape()
+				&& cards[1].getShape() == cards[2].getShape()
+				&& cards[0].getShape() == cards[2].getShape())
+				||(cards[0].getShape() != cards[1].getShape()
+				&& cards[1].getShape() != cards[2].getShape()
+				&& cards[0].getShape() != cards[2].getShape());
+		return b;
+	}
+	
+	public boolean countsEqualOrOpposite(){
+		boolean b = (cards[0].getCount() == cards[1].getCount()
+				&& cards[1].getCount() == cards[2].getCount()
+				&& cards[0].getCount() == cards[2].getCount())
+				||(cards[0].getCount() != cards[1].getCount()
+				&& cards[1].getCount() != cards[2].getCount()
+				&& cards[0].getCount() != cards[2].getCount());
+		return b;
+	}
+
+}
