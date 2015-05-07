8df7e5409d174e749bcdd3782540a1007a698233
Matthew Smarsch <matthew.smarsch@aol.com>
2015-05-07 00:26:32 -0400

    Updated Server Main Class

--- src/Communication/Server.java
+++ src/Communication/Server.java
@@ -1,34 +1,15 @@
 package Communication;
 
-import java.io.IOException;
-import java.net.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-
 /**
  * @author MSmarsch
  */
 public class Server {
-    public static ConcurrentMap<Integer, Socket> sockets = new ConcurrentHashMap<>();
-    private static int sockNum = 0;
-    public static void main(String[] args) throws IOException
+
+    public static void main(String[] args)
     {
-        try{
-            final int PORT = 3333;
-            ServerSocket SERVER = new ServerSocket(PORT);
-            System.out.println("Waiting for clients...");
-            while(true)
-            {
-                Socket SOCKET = SERVER.accept();
-                System.out.println("Client connecting from: " + SOCKET.getLocalAddress().getHostName());
-                sockNum++;
-                sockets.put(sockNum, SOCKET);
-                ServerThread serverThread = new ServerThread(sockNum, SOCKET);
-            }
-        }
-        catch(IOException e)
-        {
-            System.err.println("IOException caught.");
-        }
+        int serverPort = 5000;
+        ServerMessenger serverMessenger = new ServerMessenger();
+        MessageRouter messageRouter = new MessageRouter(serverPort, serverMessenger);
+        messageRouter.run();
     }
 }
